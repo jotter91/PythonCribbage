438a9b6fab5d82b7709b4dc8edc59f20a7daebc6
jotter91 <jjotter26@gmail.com>
2023-09-25 15:57:11 +0100

    added src path to pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 # conftest.py
-from src.pycribbage import player,discard_methods,the_play_methods,cribbage_game,cribbage_tools
+import sys
+print(sys.path)
+from pycribbage import player,discard_methods,the_play_methods,cribbage_game,cribbage_tools
 import pytest
 import pathlib
 import os 
